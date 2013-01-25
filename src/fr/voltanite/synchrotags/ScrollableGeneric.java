fe7de0f0e7ba99b6e23fd599a120fcb6d3391261
Alexandre Carchano <alexandre.carchano@etu.u-bordeaux1.fr>
2013-01-25 17:56:39 +0100

    Ceation des interfaces

---
+++ src/fr/voltanite/synchrotags/ScrollableGeneric.java
@@ -0,0 +1,42 @@
+package fr.voltanite.synchrotags;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+
+public class ScrollableGeneric extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollable_generic);
+        findViewById(R.id.retour_accueil).setOnClickListener(retouraccueil);
+        findViewById(R.id.list).setOnTouchListener(testlist);
+    }
+	
+	private final Button.OnClickListener retouraccueil = new Button.OnClickListener() {
+        public void onClick(View v) {
+          Intent intent = new Intent(getBaseContext(), MainActivity.class);
+          startActivity(intent);
+        }
+      };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_scrollable_generic, menu);
+        return true;
+    }
+    
+    public final OnTouchListener testlist = new OnTouchListener() {
+	    public boolean onTouch(View v, MotionEvent event) {
+	    	openOptionsMenu();
+			return false; 
+	    }
+	  };
+    
+}
