f407fdcc406f6397723b693fef72afe3669dc231
Alexandre Carchano <alexandre.carchano@etu.u-bordeaux1.fr>
2013-01-28 11:21:24 +0100

    Test synchro JSON (non fonctionnel)

--- src/fr/voltanite/synchrotags/MainActivity.java
+++ src/fr/voltanite/synchrotags/MainActivity.java
@@ -1,5 +1,20 @@
 package fr.voltanite.synchrotags;
 
+import java.io.IOException;
+
+import org.apache.http.*;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONObject.*;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +22,7 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
@@ -37,7 +53,7 @@ public class MainActivity extends Activity {
         }
       };
     
-      public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
     	   IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
     	   if (scanResult != null) {
     		   Intent addqrcode = new Intent(this, AddQRcode.class);
@@ -50,10 +66,40 @@ public class MainActivity extends Activity {
        
        private final Button.OnClickListener scanAnything = new Button.OnClickListener() {
            public void onClick(View v) {
-             IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
-             integrator.initiateScan();
-           }
-         };
-  
+             //IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+             //integrator.initiateScan();
+        	   Intent addqrcode = new Intent(getBaseContext(), AddQRcode.class);
+   	    	String message = "test qrcode";//scanResult.getContents();
+   	    	addqrcode.putExtra(EXTRA_MESSAGE, message);
+   	    	JSONObject jtest = new JSONObject();
+   	    	try {
+				jtest.put("batman", "truc");
+//				String url = "http://192.168.5.71:9000/iut-manager-web/bidule";
+				String url = "http://217.70.191.21/testandroidjson.php";
+				HttpResponse re = HTTPPoster.doPost(url, jtest);
+				String temp = EntityUtils.toString(re.getEntity());
+				if (temp.compareTo("SUCCESS")==0)
+				{
+				    Toast.makeText(getBaseContext(), "Sending complete!", Toast.LENGTH_LONG).show();
+				}
+				else
+				{
+					Toast.makeText(getBaseContext(), temp, Toast.LENGTH_LONG).show();
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+   	    	startActivity(addqrcode);
+           }      
+         };   
+         
+
     
 }
